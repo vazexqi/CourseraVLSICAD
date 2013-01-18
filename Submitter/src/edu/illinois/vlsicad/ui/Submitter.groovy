ac08a00e436e97a78a2aef6506f8c86347a37483
Nicholas Chen <nchen@illinois.edu>
2013-01-18 09:30:20 -0600

    Create prototype of layout
    This is the type of three-pane layout that I want for the user
    interface.

--- Submitter/src/edu/illinois/vlsicad/ui/Submitter.groovy
+++ Submitter/src/edu/illinois/vlsicad/ui/Submitter.groovy
@@ -2,23 +2,85 @@ package edu.illinois.vlsicad.ui
 
 import groovy.swing.SwingBuilder
 
-import javax.swing.WindowConstants as WC
+import java.awt.*
+
+import static javax.swing.WindowConstants.EXIT_ON_CLOSE
 
 class Submitter {
-    def swing; // The SwingBuilder that will be used to build the application
-    def frame; // The main frame for the application
-    def config; // The config reader to read general properties
+    private static final String PROPERTIES_FILE = 'resources/properties.groovy'
+
+    def swing // The SwingBuilder that will be used to build the application
+    def frame // The main frame for the application
+    def config // The config reader to read general properties
+
+    def loadAction
+    def aboutAction
 
     Submitter() {
         swing = new SwingBuilder()
-        config = new ConfigSlurper().parse(new File('resources/properties.groovy').toURI().toURL())
+        config = new ConfigSlurper().parse(new File(PROPERTIES_FILE).toURI().toURL())
+
+        // Create all actions
+        loadAction = swing.action(
+                name: 'Load Assignment',
+                closure: this.&loadAssignment,
+                mnemonic: 'O',
+        )
+        aboutAction = swing.action(
+                name: 'About',
+                closure: this.&showAbout,
+                mnemonic: 'A',
+        )
     }
 
+    // Builds the entire layout using SwingBuilder DSL
     void buildLayout() {
-        frame = swing.frame(title: config.window.title,
-                location: [100, 100],
-                size: [300, 300],
-                defaultCloseOperation: WC.EXIT_ON_CLOSE)
+        swing.edt {
+            lookAndFeel 'nimbus'
+            frame = swing.frame(title: config.window.title,
+                    location: [100, 100],
+                    minimumSize: [500, 500],
+                    defaultCloseOperation: EXIT_ON_CLOSE) {
+
+                menuBar() {
+                    menu(text: 'File') {
+                        menuItem(action: loadAction)
+                        menuItem(text: 'Quit', actionPerformed: { event -> dispose() })
+                    }
+                    menu(text: 'Help') {
+                        menuItem(action: aboutAction)
+                    }
+                }
+                vbox(size: [240, 450], constraints: BorderLayout.LINE_START, border: lineBorder(color: Color.RED)) {
+                    panel(border: compoundBorder([emptyBorder(10), titledBorder('Enter your information')])) {
+                        tableLayout {
+                            tr {
+                                td { label 'Username:' }
+                                td { textField text: 'me@example.com', id: 'username', columns: 20 }
+                            }
+                            tr {
+                                td { label 'Assignment Password' }
+                                td { textField text: 'password', id: 'password', columns: 20 }
+                            }
+                        }
+                    }
+                    panel(size: [240, 200], border: compoundBorder([emptyBorder(10), titledBorder('Complete the submission')])) {
+                        scrollPane { textArea() }
+                    }
+                }
+                vbox(size: [240, 450], constraints: BorderLayout.CENTER, border: lineBorder(color: Color.RED)) {
+                    panel(border: compoundBorder([emptyBorder(10), titledBorder('Results')])) {}
+                }
+            }
+        }
+    }
+
+    void loadAssignment(event) {
+
+    }
+
+    void showAbout(event) {
+
     }
 
     void show() {
@@ -30,4 +92,5 @@ class Submitter {
         submitter.buildLayout()
         submitter.show();
     }
+
 }
