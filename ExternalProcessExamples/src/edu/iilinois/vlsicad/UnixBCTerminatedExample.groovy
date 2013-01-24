57e95093bba91ac8089eb72cd011489a20aaf3e4
Nicholas Chen <nchen@illinois.edu>
2013-01-24 11:32:36 -0600

    Add example of using Groovy Process with BC
    Shows what happens when the process terminates because it ran out
    of time.

---
+++ ExternalProcessExamples/src/edu/iilinois/vlsicad/UnixBCTerminatedExample.groovy
@@ -0,0 +1,28 @@
+package edu.iilinois.vlsicad
+
+/**
+ * This example shows how to use the Groovy Process class to communicate with the BC command line calculator on *nix
+ * platforms.
+ */
+
+def sout = new StringBuffer() // To capture the standard output from the process
+def serr = new StringBuffer() // To capture the standard error from the process
+
+// Starts the bc CLI with -q to suppress its verbose welcome message
+// -l will load the math libraries
+Process proc = "bc -q -l".execute()
+
+proc.consumeProcessOutput(sout, serr) // Starts two threads so that standard output and standard err can be captured
+
+proc.withWriter { writer ->
+    writer << "scale = 10000\n"
+    writer << "4 * a(1)\n"
+    writer << "quit\n"
+}
+proc.waitForOrKill(1000) // Give it 1000 ms to complete or kill the process
+
+println 'Standard out captured from process: ' + sout
+println 'Standard err captured from process: ' + serr
+
+assert 0 == sout.length()
+
