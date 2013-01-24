55f209655260e30a14856603bc7af7677f8fb99e
Nicholas Chen <nchen@illinois.edu>
2013-01-24 15:52:28 -0600

    Fix typo (iilinois -> illinois)
    Things run as expected through the gradle script now.

--- ExternalProcessExamples/src/main/groovy/edu/illinois/vlsicad/UnixBCExample.groovy
+++ ExternalProcessExamples/src/main/groovy/edu/illinois/vlsicad/UnixBCExample.groovy
@@ -1,4 +1,4 @@
-package edu.iilinois.vlsicad
+package edu.illinois.vlsicad
 
 /**
  * This example shows how to use the Groovy Process class to communicate with the BC command line calculator on *nix
@@ -8,15 +8,15 @@ package edu.iilinois.vlsicad
 def sout = new StringBuffer() // To capture the standard output from the process
 def serr = new StringBuffer() // To capture the standard error from the process
 
-// Starts the bc CLI with -q to suppress its verbose welcome message
-// -l will load the math libraries
-Process proc = "bc -q -l".execute()
+Process proc = "bc -q".execute() // Starts the bc CLI with -q to suppress its verbose welcome message
 
 proc.consumeProcessOutput(sout, serr) // Starts two threads so that standard output and standard err can be captured
 
-proc.withWriter { writer ->
-    writer << "scale = 10000\n"
-    writer << "4 * a(1)\n"
+proc.withWriter {writer ->
+    writer << "a = 4\n"
+    writer << "b = 5\n"
+    writer << "c = b * a\n"
+    writer << "c\n"
     writer << "quit\n"
 }
 proc.waitForOrKill(1000) // Give it 1000 ms to complete or kill the process
@@ -24,5 +24,5 @@ proc.waitForOrKill(1000) // Give it 1000 ms to complete or kill the process
 println 'Standard out captured from process: ' + sout
 println 'Standard err captured from process: ' + serr
 
-assert 0 == sout.length()
+assert 20 == sout as int
 
