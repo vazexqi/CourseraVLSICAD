f533bf6c7a68a7c460c8df27f867d52b31499ddf
Nicholas Chen <nchen@illinois.edu>
2013-01-27 17:38:17 -0600

    Add kbdd example
    Also ignore the generated output files.
    
    See #6.

---
+++ ExternalProcessExamples/src/main/groovy/edu/illinois/vlsicad/UnixScriptKBDDInteractionsExample.groovy
@@ -0,0 +1,33 @@
+package edu.illinois.vlsicad
+
+import groovy.io.GroovyPrintWriter
+
+/**
+ * This example shows how to use the Groovy Process class to communicate with the kbdd command line tool. You need to
+ * this installed before running this program. It captures all the stdin and stdout into one string (interleaved)
+ * properly to simulate the user entering the commands and receiving feedback from bc. It does this by using the unix
+ * `script` command.
+ */
+
+def INPUT_FILE = "kbddInput.txt"
+def OUTPUT_FILE = "kbddOutput.txt"
+
+def sysout = new StringBuffer() // To capture the standard output from the process
+def syserr = new StringBuffer() // To capture the standard error from the process
+
+Process proc = "script -q ${OUTPUT_FILE} kbdd".execute() // Starts the bc CLI with -q to suppress its verbose welcome message
+
+proc.consumeProcessOutput(sysout, syserr) // Starts two threads so that standard output and standard err can be captured
+
+proc.withWriter { writer ->
+    def gWriter = new GroovyPrintWriter(writer)
+    new File(INPUT_FILE).eachLine { line ->
+        gWriter.println(line)
+    }
+}
+proc.waitForOrKill(20000) // Give it 20000 ms to complete or kill the process
+
+println 'Standard out captured from process:\n' + sysout
+println 'Standard err captured from process:\n' + syserr
+
+
