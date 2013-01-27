2461089ecbf65760f239e6229e22a5baf1b7b80d
Nicholas Chen <nchen@illinois.edu>
2013-01-27 17:27:55 -0600

    Add example showing capture of entire bc session
    We capture all the user interactions (stdin and stdout) using the unix
    `script` command.
    
    See #6.

---
+++ ExternalProcessExamples/src/main/groovy/edu/illinois/vlsicad/UnixScriptBCInteractionsExample.groovy
@@ -0,0 +1,32 @@
+package edu.illinois.vlsicad
+
+import groovy.io.GroovyPrintWriter
+
+/**
+ * This example shows how to use the Groovy Process class to communicate with the BC command line calculator on *nix
+ * platforms. It captures all the stdin and stdout into one string (interleaved) properly to simulate the user
+ * entering the commands and receiving feedback from bc. It does this by using the unix `script` command.
+ */
+
+def INPUT_FILE = "bcInput.txt"
+def OUTPUT_FILE = "bcOutput.txt"
+
+def sysout = new StringBuffer() // To capture the standard output from the process
+def syserr = new StringBuffer() // To capture the standard error from the process
+
+Process proc = "script -q ${OUTPUT_FILE} bc -q".execute() // Starts the bc CLI with -q to suppress its verbose welcome message
+
+proc.consumeProcessOutput(sysout, syserr) // Starts two threads so that standard output and standard err can be captured
+
+proc.withWriter {writer ->
+    def gWriter = new GroovyPrintWriter(writer)
+    new File(INPUT_FILE).eachLine { line ->
+        gWriter.println(line)
+    }
+}
+proc.waitForOrKill(1000) // Give it 1000 ms to complete or kill the process
+
+println 'Standard out captured from process:\n' + sysout
+println 'Standard err captured from process:\n' + syserr
+
+
