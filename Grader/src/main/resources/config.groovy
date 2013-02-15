b0d17ef375c1876e43514c88014002e30746a0f6
Nicholas Chen <nchen@illinois.edu>
2013-02-15 14:36:56 -0600

    Fix the issue where I accidentally leaked our API key
    I have changed the API key so it is no longer this old one that can
    be found through the git logs.

--- Grader/src/main/resources/config.groovy
+++ Grader/src/main/resources/config.groovy
@@ -1,4 +1,9 @@
 core {
-    coursera.API = "iqw9WQi3MgvmOJsK"
+    coursera.API = "INSERT_API_KEY_HERE"
     course.session = "vlsicad-001"
+}
+
+kbdd {
+    // Assumes that kbdd can be found in a the bin directory RELATIVE to the execution path
+    location = "./bin/kbdd"
 }
